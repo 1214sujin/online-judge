7086d3900479c545da23240f9feddd9d7402c825
Sujin Kang <102423238+1214sujin@users.noreply.github.com>
2025-03-22 14:07:05 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -6,16 +6,16 @@ it = iter(input.split('\n'))
 n, m = map(int, next(it).split())
 board = [[]] * n
 answer = 1250
-chess = [0, 1, 0, 1, 0, 1, 0, 1]
+chess = 'WBWBWBWB'
 
 for i in range(n):
-    board[i] = [1 if l == 'W' else 0 for l in next(it)]
+    board[i] = next(it)
 
 for i in range(n-7):
     for j in range(m-7):
         s = 0
         for k in range(i, i+8):
-            s += sum([board[k][j+l]^chess[l] for l in range(8)])
-            chess = [c^1 for c in chess]
+            s += sum([0 if board[k][j+l] == chess[l] else 1 for l in range(8)])
+            chess = ['W' if c == 'B' else 'B' for c in chess]
         answer = min([answer, s, 64-s])
 print(answer)
