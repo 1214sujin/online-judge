30aa60ec30410b5a9ceabaef85c9d73d111a5f86
Sujin Kang <102423238+1214sujin@users.noreply.github.com>
2025-04-18 19:24:30 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 100 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+s = {'ChongChong'}
+
+for _ in range(n):
+    a, b = sys.stdin.readline().split()
+    if a in s:
+        s.add(b)
+    elif b in s:
+        s.add(a)
+
+print(len(s))
